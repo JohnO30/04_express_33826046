1f87b3812fee223ec57ab067810871ddf91ed3f8
JohnO30 <johnoladosu@outlook.com>
2025-10-23 12:56:28 +0100

    `Refactor port variable to uppercase constant`

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000;
+
+const PORT = 8000;
 // Load the route handlers
 const mainRoutes = require("./routes/main");  
 app.use('/', mainRoutes);
